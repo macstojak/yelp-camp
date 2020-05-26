eae132aa2f0d5b633afcf0b4a00fda0ace763385
macstojak <maciej.stojak89@wp.pl>
2020-05-26 23:32:33 +0200

    change some stuff

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.HOSTNAME, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!",process.env.PORT);
 });
